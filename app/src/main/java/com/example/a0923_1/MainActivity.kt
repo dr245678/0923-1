d720373d86002e31daa2d17e3ce614dac5e2b0da
min0930 <juiklop3579@gmail.com>
2024-09-30 15:46:14 +0800

    加入樂透功能

--- app/src/main/java/com/example/a0923_1/MainActivity.kt
+++ app/src/main/java/com/example/a0923_1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.a0923_1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,9 +19,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
         var txv: TextView =
             findViewById(R.id.txv)
 
         txv.text = "0"
+    }
+
+    fun happy(v:View){
+        var txv: TextView = findViewById(R.id.txv)
+
+        txv.text = (1..100).random().toString()
     }
 }
